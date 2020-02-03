7c9b30e900a5b30885ca8e649a1cd1c0c7a4aad7
Nigel Dcruz <nigeldcruz7@gmail.com>
2020-02-03 17:53:38 +0530

    simple placeholder site created

--- js/main.js
+++ js/main.js
@@ -1,65 +1,66 @@
-
-
 // Get Page Ids
 
-const home = document.getElementById('home');
-const about = document.getElementById('about');
-const contact = document.getElementById('contact');
+const home = document.getElementById("home");
+const about = document.getElementById("about");
+const contact = document.getElementById("contact");
 
+// Get Sidebar Toggle Button
+
+const sidebarToggle = document.getElementsByClassName("toggle-sidebar")[0];
+const mainContainer = document.getElementsByClassName("main-container")[0];
 
 // Swiper Configuration
-var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    loop: false,
-    direction: 'vertical',
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: false,
+  direction: "vertical",
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'custom',
-      renderCustom: function (swiper, current, total) {
-
-        if(current == 1){
-          home.classList.add('active');
-        }
-        else{
-          home.classList.remove('active');
-        }
-        if(current == 2){
-          about.classList.add('active');
-        }
-        else{
-          about.classList.remove('active');
-        }
-        if(current == 3){
-          contact.classList.add('active');
-        }
-        else{
-          contact.classList.remove('active');
-        }
-
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    type: "custom",
+    renderCustom: function(swiper, current, total) {
+      if (current == 1) {
+        home.classList.add("active");
+      } else {
+        home.classList.remove("active");
+      }
+      if (current == 2) {
+        about.classList.add("active");
+      } else {
+        about.classList.remove("active");
+      }
+      if (current == 3) {
+        contact.classList.add("active");
+      } else {
+        contact.classList.remove("active");
+      }
     }
-    },
-  });
-
-
-  // Navigation Code
-
-  function handleNavigateToHome() {
-    mySwiper.slideTo(0, 600);
   }
+});
 
-  function handleNavigateToAbout() {
-    mySwiper.slideTo(1, 600);
-  }
+// Navigation Code
 
-  function handleNavigateToContact() {
-    mySwiper.slideTo(2, 600);
-  }
+function handleNavigateToHome() {
+  mySwiper.slideTo(0, 600);
+}
 
+function handleNavigateToAbout() {
+  mySwiper.slideTo(1, 600);
+}
 
-  home.addEventListener("click", handleNavigateToHome);
-  about.addEventListener("click", handleNavigateToAbout);
-  contact.addEventListener("click", handleNavigateToContact);
+function handleNavigateToContact() {
+  mySwiper.slideTo(2, 600);
+}
 
+home.addEventListener("click", handleNavigateToHome);
+about.addEventListener("click", handleNavigateToAbout);
+contact.addEventListener("click", handleNavigateToContact);
 
+// SideBar Toggle Code
+
+function handleToggleSidebar() {
+  mainContainer.classList.toggle("show-navigation");
+}
+
+sidebarToggle.addEventListener("click", handleToggleSidebar);
